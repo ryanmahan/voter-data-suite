1c2240ca995b69afff99c81881f09dff84d46c0b
Ryan Mahan <ryanmahan97@gmail.com>
2016-11-12 18:31:52 -0500

    This was supposed to be in the last commit
    Whoops

--- src/phones/Person.java
+++ src/phones/Person.java
@@ -1,8 +1,5 @@
 package phones;
 
-import java.io.IOException;
-import org.jsoup.Jsoup;
-
 public class Person {
     String first;
     String last;
@@ -20,30 +17,30 @@ public class Person {
     public String getLast() {
         return this.last;
     }
-
-    public String findNum() throws IllegalArgumentException, IOException, InterruptedException {
-        if (this.num == null) {
-            String html;
-            String area[] = {"508-832", "508-407", "508-729", "774-221"};
-            try {
-                html = Jsoup.connect((String)("http://www.yellowpages.com/whitepages?first=" + this.first + "&last=" + this.last + "&zip=Auburn&state=MA")).timeout(10000).get().html();
-            }
-            catch (Exception e) {
-                Thread.sleep(10000);
-                return this.findNum();
-            }
-            //TODO: Extend to (508) 832, 407, 729, (774) 221
-            //TODO: Remove possibility of getting letters in number
-            for(int i = 0 ; i < area.length ; i++){
-            	if (html.contains(area[i])) {
-                     int start = html.indexOf(area[i].substring(0, 3));
-                     this.num = html.substring(start, start + 12);
-                }
-            }
-        }
-        System.out.println(String.valueOf(this.first) + " " + this.last + " " + this.num);
-        return this.num;
-    }
+//Adding comment block until the new method in Phone Driver will be finished
+//    public String findNum() throws IllegalArgumentException, IOException, InterruptedException {
+//        if (this.num == null) {
+//            String html;
+//            String area[] = {"508-832", "508-407", "508-729", "774-221"};
+//            try {
+//                html = Jsoup.connect((String)("http://www.yellowpages.com/whitepages?first=" + this.first + "&last=" + this.last + "&zip=Auburn&state=MA")).timeout(10000).get().html();
+//            }
+//            catch (Exception e) {
+//                Thread.sleep(10000);
+//                return this.findNum();
+//            }
+//            //TODO: Extend to (508) 832, 407, 729, (774) 221
+//            //TODO: Remove possibility of getting letters in number
+//            for(int i = 0 ; i < area.length ; i++){
+//            	if (html.contains(area[i])) {
+//                     int start = html.indexOf(area[i].substring(0, 3));
+//                     this.num = html.substring(start, start + 12);
+//                }
+//            }
+//        }
+//        System.out.println(String.valueOf(this.first) + " " + this.last + " " + this.num);
+//        return this.num;
+//    }
 
     public String getNum() {
         return this.num;
