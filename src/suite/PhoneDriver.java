70891efad7f7908e432256fc049260c5dcd0e750
Ryan Mahan <ryanmahan97@gmail.com>
2016-11-15 12:30:53 -0500

    Completed new Finder methods, works
    Looks like yellowpages may have changed the way they block IP addresses
    that they think are trying to DOS them. Might have to switch to their
    paid API or a different website soon

--- src/suite/PhoneDriver.java
+++ src/suite/PhoneDriver.java
@@ -21,19 +21,19 @@ public class PhoneDriver {
         List<Person> voters = PhoneDriver.tokenizer(new File(fileName));
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yy-hh-mm-ss");
         String date = df.format(new Date());
-        System.out.println(date);
         File output = new File("output" + date + ".txt");
         output.createNewFile();
         PrintWriter out = new PrintWriter(output);
         
-        String HTML, num;
+        String HTML, num = "No phone number";
         
         for (Person p : voters) {
             HTML = HTMLGet(p);
-            num = recursivePhoneFinder(HTML);
+            num = recursivePhoneFinder(HTML, num);
             p.num = num;
-            out.println(String.valueOf(p.getFirst()) + " " + p.getLast() + " " + p.getNum());
+            out.println(String.valueOf(p.getFirst()) + " " + p.getLast() + " " + num);
             out.flush();
+            num = "No phone number";
         }
         out.close();
     }
@@ -43,7 +43,6 @@ public class PhoneDriver {
         LinkedList<Person> list = new LinkedList<Person>();
         while (names.hasNextLine()) {
             String curr = names.nextLine();
-            System.out.println(curr);
             String[] firstLast = curr.split("\t");
             list.add(new Person(firstLast[0], firstLast[1]));
         }
@@ -54,35 +53,38 @@ public class PhoneDriver {
     public static String HTMLGet(Person p) throws InterruptedException{
     	String html = null;
         try {
-            html = Jsoup.connect((String)("http://www.yellowpages.com/whitepages?first=" + p.first + "&last=" + p.last + "&zip=Auburn&state=MA")).timeout(10000).get().html();
+        	System.out.println("HTML Loop");
+            html = Jsoup.connect((String)("http://people.yellowpages.com/whitepages?first=" + p.first + "&last=" + p.last + "&zip=auburn&state=ma&site=79")).timeout(2000).get().html();
         }
         catch (Exception e) {
+        	System.out.println("error? " + e);
 			Thread.sleep(10000);
+			html = HTMLGet(p);
             return html;
         }
+        System.out.println("HTML Got");
         return html;
     }
     
-    public static String recursivePhoneFinder(String text){
-    	String area[] = {"508-832", "508-407", "508-729", "774-221"};
-    	String num = "No phone number";
-    	if(text == null)
-    		throw new IllegalArgumentException("no HTML found for number");
-    	
+    public static String recursivePhoneFinder(String text, String num){
+    	String area[] = {"(508) 832", "(508) 407", "(508) 729", "(774) 221", "(508) 721"};
+    	System.out.println("Recursive loop");
     	for(int i = 0 ; i < area.length ; i++){
-        	if (text.contains(area[i])) {
+    		
+        	if (text.contains(area[i]) == true) {
                  int start = text.indexOf(area[i].substring(0, 3));
-                 num = text.substring(start, start + 12);
+                 num = text.substring(start-1, start + 14);
+                 
             }
         }
-    	
+
     	if(num == "No phone number"){
     		return num;
     	}
     	
     	if(num.matches(".*[a-zA-Z]+.*") && text.length() > 0){
     		num = num.substring(1, num.length());
-    		num = recursivePhoneFinder(num);
+    		num = recursivePhoneFinder(text, num);
     	}
     	
     	return num;
