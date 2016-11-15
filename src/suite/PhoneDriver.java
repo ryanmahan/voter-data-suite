76b0b188a62628377e3ce1fb267ac1465b46df8c
Ryan Mahan <ryanmahan97@gmail.com>
2016-11-15 13:58:16 -0500

    Added more comments

--- src/suite/PhoneDriver.java
+++ src/suite/PhoneDriver.java
@@ -12,6 +12,9 @@ import org.jsoup.Jsoup;
 
 import suite.Person;
 
+
+
+
 public class PhoneDriver {
     public static void main(String[] args) throws Exception {
         Scanner user = new Scanner(System.in);
@@ -37,23 +40,43 @@ public class PhoneDriver {
         }
         out.close();
     }
-
+    /* Tokenizer
+     * @returns a LinkedList of Person objects, filled in with info from a file
+     * @param a file of tab-delimited names "FIRST	LAST"
+     * 
+     * Runs through every line in the file, splitting the line on tabs, then creates a new person object
+     * and adds person object to the list
+     */
     public static List<Person> tokenizer(File f) throws FileNotFoundException {
+    	
         Scanner names = new Scanner(f);
         LinkedList<Person> list = new LinkedList<Person>();
+        
         while (names.hasNextLine()) {
+        	
             String curr = names.nextLine();
             String[] firstLast = curr.split("\t");
             list.add(new Person(firstLast[0], firstLast[1]));
+            
         }
+        
         names.close();
         return list;
     }
-    
+    /* HTMLGet
+     * @returns a large HTML file formatted as a string
+     * @param person object with the names filled in at the very least
+     * 
+     * Uses the JSoup API to download a HTML file from yellowpages
+     * if the API returns an error (most likely a connection error from yellowpages blocking the IP)
+     * 	Print the error, sleep for 10 seconds, then recursively try again with the same name, eventually returning the html file
+     * 
+     * if no error, return this HTML file as string
+     * 
+     */
     public static String HTMLGet(Person p) throws InterruptedException{
     	String html = null;
         try {
-        	System.out.println("HTML Loop");
             html = Jsoup.connect((String)("http://people.yellowpages.com/whitepages?first=" + p.first + "&last=" + p.last + "&zip=auburn&state=ma&site=79")).timeout(2000).get().html();
         }
         catch (Exception e) {
@@ -62,13 +85,23 @@ public class PhoneDriver {
 			html = HTMLGet(p);
             return html;
         }
-        System.out.println("HTML Got");
+       
         return html;
     }
     
+    /* recursivePhoneFinder
+     * @returns String of the phone number of the HTML provided as a param
+     * @param String Text - the HTML file as string provided by HTMLGet or a string to search through
+     * @param num - the target num as a string
+     * 
+     * Searches for the phone numbers in Area, runs a loop over all areas, tests if its in the html input, then isolates it
+     * if its not found, or if a text with these numbers but incorrect, it gets rid of the current finding by moving forward one
+     * then running the function again. Does this until no area codes are found, then returns "No phone number"
+     */
+    
     public static String recursivePhoneFinder(String text, String num){
     	String area[] = {"(508) 832", "(508) 407", "(508) 729", "(774) 221", "(508) 721"};
-    	System.out.println("Recursive loop");
+    	//System.out.println("Recursive loop");
     	for(int i = 0 ; i < area.length ; i++){
     		
         	if (text.contains(area[i]) == true) {
